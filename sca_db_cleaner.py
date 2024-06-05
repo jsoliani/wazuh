d1a33df58e9ed08e4feaa16d1f300729397ad2a9
John Soliani <juanpablosoliani@gmail.com>
2024-06-05 14:53:22 -0300

    Update sca_db_cleaner.py

--- sca_db_cleaner.py
+++ sca_db_cleaner.py
@@ -1,4 +1,4 @@
-#! /var/ossec/framework/python/bin/python3
+#!/var/ossec/framework/python/bin/python3
 
 from socket import socket, AF_UNIX, SOCK_STREAM
 from struct import pack, unpack
@@ -22,7 +22,7 @@ def db_query(agent, query):
     return sock.recv(length).decode(errors='ignore')
 
 def pretty(response):
-    if response.startswith('ok '):
+    if (response.startswith('ok ')):
         try:
             data = loads(response[3:])
             return dumps(data, indent=4)
@@ -32,24 +32,25 @@ def pretty(response):
         return response
 
 if __name__ == "__main__":
-
-    log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [INFO] SCA DB cleaner script started.{filename}\n')
+    log_file_path = '/var/ossec/logs/sca_db_cleaner.log'
     db_files = [pos_db for pos_db in os.listdir(db_folder) if pos_db.endswith('.db') and pos_db != 'global.db' and pos_db != '.template.db' and pos_db != '000.db']
-    logfile = '/var/ossec/logs/sca_db_cleaner.log'
-    for filename in db_files:
-        print("Cleaning SCA last scans of AgentID: " + filename[0:len(filename)-3])
-        response1 = db_query(filename[0:len(filename)-3], "DELETE FROM sca_scan_info;")
-        response2 = db_query(filename[0:len(filename)-3], "DELETE FROM sca_check;")
-        if Path(logfile).is_file():
-            log_file = open(logfile, "a+")
-        else:
-            log_file = open(logfile, "w")
-        if pretty(response1) == '[]':
-            log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [INFO] First sca table cleaned! Agent\'s file: {filename}\n')
-        else:
-            log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [ERROR] Something went wrong! {response1} Agent\'s file: {filename}\n')
-        if pretty(response2) == '[]':
-            log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [INFO] Second sca table cleaned! Agent\'s file: {filename}\n')
-        else:
-            log_file.write(f'{datetime.datetime.now()} - vd_db_cleaner: [ERROR] Something went wrong with the second table! {response2} Agent\'s file: {filename}\n')
 
+    with open(log_file_path, "a+") as log_file:
+        log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [INFO] SCA DB cleaner script started.\n')
+
+        for filename in db_files:
+            agent_id = filename[:-3]  # Extract the agent ID from the filename
+            print("Cleaning SCA last scans of AgentID: " + agent_id)
+
+            response1 = db_query(agent_id, "DELETE FROM sca_scan_info;")
+            response2 = db_query(agent_id, "DELETE FROM sca_check;")
+
+            if pretty(response1) == '[]':
+                log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [INFO] First SCA table cleaned! Agent\'s file: {filename}\n')
+            else:
+                log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [ERROR] Something went wrong! {response1} Agent\'s file: {filename}\n')
+
+            if pretty(response2) == '[]':
+                log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [INFO] Second SCA table cleaned! Agent\'s file: {filename}\n')
+            else:
+                log_file.write(f'{datetime.datetime.now()} - sca_db_cleaner: [ERROR] Something went wrong with the second table! {response2} Agent\'s file: {filename}\n')
